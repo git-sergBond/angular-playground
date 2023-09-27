d2848fca0c4cf33db83fff500da2a8932ecef57d
git-sergBond <git-serg.bond@yandex.ru>
2023-09-28 02:08:55 +0300

    router

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from "rxjs";
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,18 @@ export class HeaderComponent {
   showAddTask: boolean = false;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(
+    private uiService: UiService,
+    private router: Router
+  ) {
     this.subscription = uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string): boolean {
+    return this.router.url === route;
   }
 }
